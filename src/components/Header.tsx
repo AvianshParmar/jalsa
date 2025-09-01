e691c6c0f889ded2b3eb73c8a011c1ae2093c335
Avinash Parmar <89641195+AvianshParmar@users.noreply.github.com>
2025-09-01 19:55:03 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,7 +61,7 @@ const Header = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-3">
             <img 
-              src={logo} 
+              src={process.env.PUBLIC_URL + "/png_logo.png"}  
               alt="Jalsa Paan Logo" 
               className="h-10 w-auto md:h-16 md:w-25 "
             />
